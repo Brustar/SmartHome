b29b84057a269c26288ccd543c20fb9a5f165ed6
“xiaohuangren” <568066184@qq.com>
2016-07-05 11:59:49 +0800

    d

--- SmartHome/RegisterDetailController.m
+++ SmartHome/RegisterDetailController.m
@@ -18,6 +18,11 @@
 @property (weak, nonatomic) IBOutlet UITextField *passWord;
 @property (weak, nonatomic) IBOutlet UITextField *pwdAgain;
 @property (nonatomic,assign) int cType;
+
+@property (weak, nonatomic) IBOutlet UIButton *auothCodeBtn;
+
+@property (nonatomic,strong) dispatch_source_t _timer;
+
 @end
 
 @implementation RegisterDetailController
@@ -32,7 +37,46 @@
     }else self.cType = 1;
 }
 
+#pragma  mark - 手机验证码
+- (IBAction)sendAuothCode:(id)sender {
+    __block int timeout=30; //倒计时时间
+    dispatch_queue_t queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
+    self._timer = dispatch_source_create(DISPATCH_SOURCE_TYPE_TIMER, 0, 0,queue);
+    dispatch_source_set_timer(self._timer,dispatch_walltime(NULL, 0),1.0*NSEC_PER_SEC, 0); //每秒执行
+    dispatch_source_set_event_handler(self._timer, ^{
+        if(timeout<=0){ //倒计时结束，关闭
+            dispatch_source_cancel(self._timer);
+            dispatch_async(dispatch_get_main_queue(), ^{
+               
+                [self.auothCodeBtn setTitle:@"获取验证码" forState:UIControlStateNormal];
+                self.auothCodeBtn.userInteractionEnabled = YES;
+            });
+        }else{
+            int seconds = timeout % 60;
+            NSString *strTime = [NSString stringWithFormat:@" %.2d", seconds];
+            dispatch_async(dispatch_get_main_queue(), ^{
+                
+                [UIView beginAnimations:nil context:nil];
+                [UIView setAnimationDuration:1];
+                [self.auothCodeBtn setTitle:[NSString stringWithFormat:@"重新获取(%@) ",strTime] forState:UIControlStateNormal];
+                [UIView commitAnimations];
+                self.auothCodeBtn.userInteractionEnabled = NO;
+            });
+            timeout--;
+        }
+    });
+    dispatch_resume(self._timer);
+
+}
+
+
 - (IBAction)clickRegisterBtn:(id)sender {
+    if([self.authorNum.text isEqualToString:@""]|| [self.userName.text isEqualToString:@""]||[self.passWord.text isEqualToString:@""])
+    {
+        [DialogManager showMessage:@"信息不能为空"];
+        return;
+    }
+    
     if(![self.passWord.text isEqualToString:self.pwdAgain.text])
     {
         [DialogManager showMessage:@"两次密码不匹配"];
