9f46a4a5b95516add893f77287bfbbc9062a855a
“Brustar” <yeals2@gmail.com>
2016-08-01 10:11:16 +0800

    init

--- SmartHome/AppDelegate.m
+++ SmartHome/AppDelegate.m
@@ -48,9 +48,9 @@
         [[SocketManager defaultManager] connectUDP:[IOManager udpPort]];
     }
     //登录后每次系统启动自动更新云端配置，第一次安装此处不更新，登录的时候再更新
-    //if ([[NSUserDefaults standardUserDefaults] objectForKey:@"masterID"]) {
+    if ([[NSUserDefaults standardUserDefaults] objectForKey:@"masterID"]) {
         [device initConfig];
-    //}
+    }
     
     return YES;
 }
