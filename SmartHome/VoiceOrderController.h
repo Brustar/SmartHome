1661ff2e217242a52374c8564d72a3e9808bcc93
“Brustar” <yeals2@gmail.com>
2016-08-24 18:16:03 +0800

    voice

--- SmartHome/VoiceOrderController.h
+++ SmartHome/VoiceOrderController.h
@@ -6,8 +6,20 @@
 //  Copyright © 2016年 Brustar. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import "iflyMSC/IFlySpeechSynthesizerDelegate.h"
+#import "iflyMSC/IFlySpeechRecognizerDelegate.h"
+//引入语音合成类
+@class IFlySpeechSynthesizer;
+@class IFlyDataUploader;
+@class IFlySpeechRecognizer;
 
-@interface VoiceOrderController : UIViewController
+@interface VoiceOrderController : UIViewController<IFlySpeechSynthesizerDelegate,IFlySpeechRecognizerDelegate>
+
+
+//声明语音合成的对象
+@property (nonatomic, strong) IFlySpeechSynthesizer *iFlySpeechSynthesizer;
+@property (strong, nonatomic) IBOutlet UITextField *content;
+@property (nonatomic, strong) IFlySpeechRecognizer *iFlySpeechRecognizer;//不带界面的识别对象
+@property (strong, nonatomic) IBOutlet UITextView *textView;
 
 @end
--- SmartHome/VoiceOrderController.m
+++ SmartHome/VoiceOrderController.m
@@ -7,6 +7,11 @@
 //
 
 #import "VoiceOrderController.h"
+#import "iflyMSC/IFlySpeechConstant.h"
+#import "iflyMSC/IFlySpeechUtility.h"
+#import "iflyMSC/IFlySpeechSynthesizer.h"
+#import "iflyMSC/IFlySpeechRecognizer.h"
+#import "IATConfig.h"
 
 @interface VoiceOrderController ()
 
@@ -18,7 +23,81 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
 }
+
+-(void)initRecognizer
+{
+    NSLog(@"%s",__func__);
+    
+    //单例模式，无UI的实例
+    if (_iFlySpeechRecognizer == nil) {
+        _iFlySpeechRecognizer = [IFlySpeechRecognizer sharedInstance];
+        
+        [_iFlySpeechRecognizer setParameter:@"" forKey:[IFlySpeechConstant PARAMS]];
+        
+        //设置听写模式
+        [_iFlySpeechRecognizer setParameter:@"iat" forKey:[IFlySpeechConstant IFLY_DOMAIN]];
+    }
+    _iFlySpeechRecognizer.delegate = self;
+    
+    if (_iFlySpeechRecognizer != nil) {
+        IATConfig *instance = [IATConfig sharedInstance];
+        
+        //设置最长录音时间
+        [_iFlySpeechRecognizer setParameter:instance.speechTimeout forKey:[IFlySpeechConstant SPEECH_TIMEOUT]];
+        //设置后端点
+        [_iFlySpeechRecognizer setParameter:instance.vadEos forKey:[IFlySpeechConstant VAD_EOS]];
+        //设置前端点
+        [_iFlySpeechRecognizer setParameter:instance.vadBos forKey:[IFlySpeechConstant VAD_BOS]];
+        //网络等待时间
+        [_iFlySpeechRecognizer setParameter:@"20000" forKey:[IFlySpeechConstant NET_TIMEOUT]];
+        
+        //设置采样率，推荐使用16K
+        [_iFlySpeechRecognizer setParameter:instance.sampleRate forKey:[IFlySpeechConstant SAMPLE_RATE]];
+        
+        if ([instance.language isEqualToString:[IATConfig chinese]]) {
+            //设置语言
+            [_iFlySpeechRecognizer setParameter:instance.language forKey:[IFlySpeechConstant LANGUAGE]];
+            //设置方言
+            [_iFlySpeechRecognizer setParameter:instance.accent forKey:[IFlySpeechConstant ACCENT]];
+        }else if ([instance.language isEqualToString:[IATConfig english]]) {
+            [_iFlySpeechRecognizer setParameter:instance.language forKey:[IFlySpeechConstant LANGUAGE]];
+        }
+        //设置是否返回标点符号
+        [_iFlySpeechRecognizer setParameter:instance.dot forKey:[IFlySpeechConstant ASR_PTT]];
+        
+    }
+}
+
 - (IBAction)startVoice:(id)sender {
+    [_textView setText:@""];
+    [_textView resignFirstResponder];
+    //self.isCanceled = NO;
+    
+    if(_iFlySpeechRecognizer == nil)
+    {
+        [self initRecognizer];
+    }
+    
+    [_iFlySpeechRecognizer cancel];
+    
+    //设置音频来源为麦克风
+    [_iFlySpeechRecognizer setParameter:IFLY_AUDIO_SOURCE_MIC forKey:@"audio_source"];
+    
+    //设置听写结果格式为json
+    [_iFlySpeechRecognizer setParameter:@"json" forKey:[IFlySpeechConstant RESULT_TYPE]];
+    
+    //保存录音文件，保存在sdk工作路径中，如未设置工作路径，则默认保存在library/cache下
+    [_iFlySpeechRecognizer setParameter:@"asr.pcm" forKey:[IFlySpeechConstant ASR_AUDIO_PATH]];
+    
+    [_iFlySpeechRecognizer setDelegate:self];
+    
+    BOOL ret = [_iFlySpeechRecognizer startListening];
+    
+    if (ret) {
+        
+    }else{
+        NSLog(@"启动识别服务失败，请稍后重试");//可能是上次请求未结束，暂不支持多路并发
+    }
 }
 
 - (void)didReceiveMemoryWarning {
@@ -26,5 +105,66 @@
     // Dispose of any resources that can be recreated.
 }
 
+#pragma mark - IFlySpeechRecognizerDelegate
+- (void) onError:(IFlySpeechError *) error
+{
+}
+
+- (void) onResults:(NSArray *) results isLast:(BOOL)isLast
+{
+    
+    NSMutableString *resultString = [[NSMutableString alloc] init];
+    NSDictionary *dic = results[0];
+    for (NSString *key in dic) {
+        [resultString appendFormat:@"%@",key];
+    }
+    NSString *result =[NSString stringWithFormat:@"%@%@", _textView.text,resultString];
+    NSString * resultFromJson =  [NSJSONSerialization JSONObjectWithData:[resultString dataUsingEncoding:NSUTF8StringEncoding] options:NSJSONReadingMutableContainers error:nil];
+    _textView.text = [NSString stringWithFormat:@"%@%@", _textView.text,resultFromJson];
+    
+    if (isLast){
+        NSLog(@"听写结果(json)：%@测试", result);
+    }
+    NSLog(@"_result=%@",result);
+    NSLog(@"resultFromJson=%@",resultFromJson);
+    NSLog(@"isLast=%d,_textView.text=%@",isLast,_textView.text);
+}
+
+#pragma mark - IFlySpeechSynthesizerDelegate
+//开始播放
+- (void) onSpeakBegin
+{
+    
+}
+
+//缓冲进度
+- (void) onBufferProgress:(int) progress message:(NSString *)msg
+{
+    NSLog(@"bufferProgress:%d,message:%@",progress,msg);
+}
+
+//播放进度
+- (void) onSpeakProgress:(int) progress
+{
+    NSLog(@"play progress:%d",progress);
+}
+
+//暂停播放
+- (void) onSpeakPaused
+{
+    
+}
+
+//恢复播放
+- (void) onSpeakResumed
+{
+    
+}
+
+//结束回调
+- (void) onCompleted:(IFlySpeechError *) error
+{
+    
+}
 
 @end
