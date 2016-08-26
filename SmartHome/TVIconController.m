7bbd1d3b06425e41ed06832eff7f73a545ee8b91
“xiaohuangren” <568066184@qq.com>
2016-08-26 20:13:45 +0800

    s

---
+++ SmartHome/TVIconController.m
@@ -0,0 +1,37 @@
+//
+//  TVIconController.m
+//  SmartHome
+//
+//  Created by 逸云科技 on 16/8/26.
+//  Copyright © 2016年 Brustar. All rights reserved.
+//
+
+#import "TVIconController.h"
+
+@interface TVIconController ()
+
+@end
+
+@implementation TVIconController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
