f9fce6c93452021786d0c89707f077acda206648
“Brustar” <yeals2@gmail.com>
2016-11-16 16:51:54 +0800

    logout

--- SmartHome/IphoneControllers/IphoneProfileController.m
+++ SmartHome/IphoneControllers/IphoneProfileController.m
@@ -78,7 +78,8 @@
     UIAlertController * alertVC = [UIAlertController alertControllerWithTitle:@"退出后不会删除任何数据，下次依然可以使用本账号" message:nil preferredStyle:UIAlertControllerStyleActionSheet];
     UIAlertAction * action = [UIAlertAction actionWithTitle:@"退出登录" style:UIAlertActionStyleDestructive handler:^(UIAlertAction * _Nonnull action) {
         
-        [self clickQuitButton];
+        //[self clickQuitButton];
+        [self performSegueWithIdentifier:@"iphoneQuitSegue" sender:self];
     }];
     
     UIAlertAction * action1 = [UIAlertAction actionWithTitle:@"取消" style:UIAlertActionStyleCancel handler:^(UIAlertAction * _Nonnull action) {
