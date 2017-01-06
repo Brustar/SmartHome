3e9809c1865a682dccd8edc8e23398d0eae98e1c
“Brustar” <yeals2@gmail.com>
2017-01-06 14:17:29 +0800

    remove homekit

--- SmartHome/PluginViewController.h
+++ SmartHome/PluginViewController.h
@@ -5,19 +5,21 @@
 //  Created by 逸云科技 on 16/8/5.
 //  Copyright © 2016年 Brustar. All rights reserved.
 //
-#import <HomeKit/HomeKit.h>
+//#import <HomeKit/HomeKit.h>
 
 #define DEVICE_TYPE @"智能单品"
 
-@interface PluginViewController : UIViewController<HMHomeManagerDelegate, HMHomeDelegate>
+@interface PluginViewController : UIViewController//<HMHomeManagerDelegate, HMHomeDelegate>
 
 @property (strong, nonatomic) NSMutableArray *devices;
 @property (nonatomic,assign) int roomID;
 @property (nonatomic,weak) NSString *sceneid;
 @property (nonatomic,weak) NSString *deviceid;
+/*
 @property (nonatomic, strong) HMHomeManager *homeManager;
 @property (nonatomic, strong) HMHome *primaryHome;
 @property (nonatomic, strong) HMCharacteristic *characteristic;
+*/
 @property (nonatomic,assign) BOOL isAddDevice;
 
 @property (strong, nonatomic) Scene *scene;
