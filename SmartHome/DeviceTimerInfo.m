1a8dfe6c38fa69707a02719c52c50bd1013daa67
KobeBryant2016 <18826575727@126.com>
2017-02-28 08:45:51 +0800

    regular

---
+++ SmartHome/DeviceTimerInfo.m
@@ -0,0 +1,14 @@
+//
+//  DeviceTimerInfo.m
+//  SmartHome
+//
+//  Created by KobeBryant on 2017/2/27.
+//  Copyright © 2017年 Brustar. All rights reserved.
+//
+
+#import "DeviceTimerInfo.h"
+
+@implementation DeviceTimerInfo
+
+
+@end
