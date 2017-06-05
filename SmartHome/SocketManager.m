571107c6e33e8b349bd05147eff806d4272007f8
“Brustar” <yeals2@gmail.com>
2017-06-05 19:14:14 +0800

    sock

--- SmartHome/SocketManager.m
+++ SmartHome/SocketManager.m
@@ -52,10 +52,10 @@
 
 -(void)connectUDP:(int)port
 {
-    /*if ([NetStatusManager isEnableWWAN]) {
+    if (![NetStatusManager isEnableWIFI]) {
         [self connectTcp];
         return;
-    }*/
+    }
     [self connectUDP:port delegate:self];
 }
 
